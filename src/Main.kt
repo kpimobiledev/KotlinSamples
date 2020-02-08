4f470fe8a3c4fc4f52c06b1c80cbdc5727ee8d99
lidaamber <lidaamber@gmail.com>
2020-02-08 20:24:07 +0200

    chars

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,13 @@
 fun main() {
-    val isMorning = true
-    var isAwake = false
+    val myChar = 'a'
+    val tabChar = '\t'
+    val unicodeChar = '\uFF00'
 
-    if (isMorning && !isAwake) println("Bring me coffee")
+    // Chars and Ints compatibility
+    println("Comparing Char to Int: ${myChar.toInt() == 1}") // Try to remove toInt()
 
-    isAwake = true // Try input 1 instead of true
-
-    if (isMorning || isAwake) println("I went to the lecture anyway")
-
+    // Chars and ranges
+    println("$myChar is from a to z: ${myChar in 'a'..'z'}")
+    println("$unicodeChar is not from 1 to 9: ${unicodeChar !in '1'..'9'}")
 }
 
