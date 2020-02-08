573fe5b72b33ecc3fec1c43403c1fd3a51bf2cde
lidaamber <lidaamber@gmail.com>
2020-02-08 22:04:00 +0200

    while & do-while loops

--- src/Main.kt
+++ src/Main.kt
@@ -1,40 +1,19 @@
 fun main() {
     val ints = intArrayOf(1, 2, 3, 4, 5)
 
-    // For loop
-    println("For-in loop")
-    for (element in ints) {
-        println(element)
+    // While loop
+    println("While loop")
+    var lastIndex = ints.size
+    while (lastIndex != 0) {
+        lastIndex--
+        println(ints[lastIndex])
     }
 
-    // For loop with indices
-    println("For-in loop with indices")
-    for (index in ints.indices) {
-        println(ints[index])
-    }
-
-    // For loop using range
-    println("For-in loop with range")
-    for (index in 0 until ints.size) {
-        println(ints[index])
-    }
-
-    // For loop using range (other way)
-    println("For-in loop with range x2")
-    for (index in 0..ints.size - 1) {
-        println(ints[index])
-    }
-
-    // For loop with range and step
-    println("For-in loop with steps and range")
-    for (index in 10 downTo 0 step 2) {
-        println(index)
-    }
-
-    // For loop with pair of index-value
-    println("For-in loop with index-value pair")
-    for ((index, value) in ints.withIndex()) {
-        println("Value at $index is $value")
-    }
+    // Do-While loop
+    println("Do-while loop")
+    do {
+        println(ints[lastIndex])
+        lastIndex++
+    } while (lastIndex < ints.size)
 }
 
