eec4f73b8885a04adfff3f0feb0b1f983bb422e8
lidaamber <lidaamber@gmail.com>
2020-02-10 19:41:36 +0200

    objects

--- src/Main.kt
+++ src/Main.kt
@@ -1,37 +1,75 @@
 fun main() {
-    var animal = Animal.CAT
-    animal = Animal.DOG
-
-    var currency = Currency.UAH
-    currency.someOtherValue = "value"
-    currency.printSymbol()
-
-    // When-expression doesn't require else if all enum values are options
-    val message = when (currency) {
-        Currency.UAH -> "Hryvnia"
-        Currency.EUR -> "Euro"
-        Currency.USD -> "Dollar"
+    // Object expression implementing an interface
+    val myPrintable = object : Printable {
+        override fun print() {
+            println("I can be printed")
+        }
     }
 
-    println(message)
+    // Object expression inheriting a class
+    val myAnimation = object : Animation() {
+        override fun animate() {
+            println("Animation running")
+        }
+    }
+
+    // Using object declaration
+    println(SingletonObject.someValue)
+    SingletonObject.someMethod()
+
+    // Using a function with an object expression
+    doSomething()
+
+    // Using an object returned by a private function
+    val generatedObject = generateObject("John", 20)
+    println(generatedObject.personName)
+
+    // Calling method of the companion class
+    SomeClass.printPI()
+    SomeClass.Companion.printPI()
+
 }
 
-// Simple enum class
-enum class Animal {
-    CAT,
-    DOG
+// Dummy interface
+interface Printable {
+    fun print()
 }
 
-// Enum class with constructor, additional value and a method
-enum class Currency(private val symbol: Char) {
-    USD('$'),
-    UAH('₴'),
-    EUR('€');
+// Dummy open class
+open class Animation {
+    open fun animate() {}
+}
 
-    var someOtherValue: String? = null
+// Object declaration
+object SingletonObject {
+    var someValue: String? = "Value"
 
-    fun printSymbol() {
-        print(symbol)
+    fun someMethod() {
+        println("Hello from Singleton")
     }
 }
 
+fun doSomething() {
+    // Object can be created as a variable
+    val address = object {
+        var city = "Kyiv"
+        val country = "Ukraine"
+    }
+
+    println("${address.city}, ${address.country}")
+}
+
+// Object can be returned by private function
+private fun generateObject(name: String, age: Int) = object {
+    val personName = name
+    val personAge = age
+}
+
+// Companion object example
+class SomeClass {
+    companion object {
+        const val PI = 3.14 // Static value
+
+        fun printPI() = println(PI) // Static method
+    }
+}
