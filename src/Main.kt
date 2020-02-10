d7aff62edaf5803c8d8f1967ac44f716781f29f5
lidaamber <lidaamber@gmail.com>
2020-02-10 18:16:53 +0200

    visibility modifiers

--- src/Main.kt
+++ src/Main.kt
@@ -1,36 +1,17 @@
 fun main() {
-    val inner = Outer().Inner()
-    inner.innerMethod()
+    // Playing with same file modifiers
+    val sameFileSample = SameFileSample()
+    sameFileSample.usePublicClass()
+    sameFileSample.useInternalClass()
+    sameFileSample.usePrivateClass()
+    sameFileSample.useValuesAndFunctions()
+    sameFileSample.checkNestedAndInnerClasses()
 
-    val nested = Outer.Nested()
-    nested.nestedMethod()
-}
-
-// Simple outer class with a few fields and methods
-class Outer {
-    private val outerVal = 100
-    var outerVar = "Hello"
-
-    fun outerMethod() { println("Do something") }
-
-    private fun otherOuterMethod() { println("Do something secretive") }
-
-    inner class Inner {
-        fun innerMethod() {
-            println(outerVal)
-            println(outerVar)
-            outerMethod()
-            otherOuterMethod()
-        }
-    }
-
-    class Nested {
-        fun nestedMethod() {
-            // Try to uncomment it
-            // println(outerVal)
-            // println(outerVar)
-            // outerMethod()
-            // otherOuterMethod()
-        }
-    }
+    // Playing with same package modifiers
+    val samePackageSample = SamePackageSample()
+    samePackageSample.usePublicClass()
+    samePackageSample.useInternalClass()
+    samePackageSample.usePrivateClass()
+    samePackageSample.useValuesAndFunctions()
+    samePackageSample.checkNestedAndInnerClasses()
 }
