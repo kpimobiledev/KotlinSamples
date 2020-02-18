355f04ad939d95cad7dea1122bbe3a6f0cd84619
lidaamber <lidaamber@gmail.com>
2020-02-18 17:38:30 +0200

    filtering operations

--- src/Main.kt
+++ src/Main.kt
@@ -1,72 +1,43 @@
 fun main() {
 
-    // Mapping: transformation of one collection to another by transforming collection values
-
     val namesList = arrayListOf("Alice", "Bob")
     val optionalNamesSet = setOf("Alice", null, "Bob")
     val gradesMap = mapOf("Alice" to 100, "Bob" to 95)
 
-    // Converting to a list of element lengths
-    val lengthsList = namesList.map { it.length }
+    val anyList = listOf(1, 'a', "Alice")
 
-    // Converting to a list of index-value pairs and then to map
-    val indexedNamesMap = namesList.mapIndexed { index, s -> index to s }.toMap()
+    // Filtering examples with predicates
+    namesList.filter { it.contains("i") }.forEach { println(it) }
+    gradesMap.filter { (key, value) -> key.contains('A') && value > 90 }
 
-    // Mapping optional collections
-    val lengthsSet = optionalNamesSet.mapNotNull { it?.length }
-    val nameLengthMap = optionalNamesSet.mapIndexedNotNull { index, s -> index to s?.length }.toMap()
+    // Filtering with index
+    namesList.filterIndexed { index, value -> index % 2 == 0 && value.contains('A') }
 
-    // Converting to a list of index-value pairs and then to map
-    val examGradesMap = gradesMap.map { it.key to it.value * 0.6f }.toMap()
+    // Filtering optional collection
+    optionalNamesSet.filter { return@filter if (it == null) false else it.length > 5 }
 
-    // Transforming map to list
-    val valuesList = gradesMap.map { it.value }
+    // Filtering by not matching the predicate
+    namesList.filterNot { it == it.toUpperCase() }
 
-    // Transforming keys to the mapped values
-    val mappedKeysMap = gradesMap.mapKeys { "${it.key} ${it.value}" }
+    // Filtering an optional collection to receive non-null values
+    optionalNamesSet.filterNotNull()
 
-    // Transforming values to the mapped values
-    val mappedValuesMap = gradesMap.mapValues { "${it.key} ${it.value}" }
+    // Filtering Any collection to receive only String values
+    anyList.filterIsInstance<String>()
 
-    // Zipping: transformation of mapping two collections values by index
+    // Partitioning: splitting the collection to two collections by predicate
+    val (shortNamesList, longNamesList) = namesList.partition { it.length > 5 }
 
-    val firstWords = listOf("Roses", "Violets")
-    val secondWords = listOf("Red", "Blue")
+    // Filtering checks: any, none, all
+    println(namesList.any { it.startsWith('A') })
+    println(namesList.none { it.length > 10 })
+    println(namesList.all { it.toUpperCase() != it })
 
-    firstWords.zip(secondWords).forEach { println("${it.first} are ${it.second}") }
+    val emptyList = emptyList<String>()
 
-    val somePairsList = listOf("Alice" to 100, "Bob" to 95)
-    val (names, grades) = somePairsList.unzip()
+    // Checking if the collection has any items
+    println(emptyList.any())
 
-    // Association: building maps from collections
-
-    // Creation of map with names as keys and lengths as values
-    val lengthValuesMap = namesList.associateWith { it.length }
-
-    // Creation of map with names as values and length as keys
-    val lengthKeysMap = namesList.associateBy { it.length }
-
-    // Association with both values and keys changing
-    val namesAssociatedMap = namesList.associate { it.toUpperCase() to it.length }
-
-    // Flattening: creation of one-level collection from the elements which are collections
-    val collectionOfCollections = listOf(setOf(1, 2, 3), listOf(4, 5, 6))
-    val collectionsMap = mapOf("Alice" to listOf("Singing", "Dancing"), "Bob" to listOf("Playing drums"))
-
-    // Flattening the collection of collections to a one-level collection
-    collectionOfCollections.flatten().forEach { println(it) }
-
-    // Using flatMap to flatten the collections
-    collectionOfCollections.flatMap { it.toList() }.forEach { println(it) }
-    collectionsMap.flatMap { it.value }.forEach { println(it) }
-
-    // String joining: transformation of the collection elements to String
-
-    // Joining list elements to String using default settings and customized settings
-    println(namesList.joinToString())
-    println(optionalNamesSet.joinToString(separator = " * ", prefix = "!!! ", postfix = "%%%"))
-
-    // Joining list elements to StringBuffer
-    val namesBuffer = StringBuffer("Names: ")
-    println(namesList.joinTo(namesBuffer))
+    // Checking if the collection has no items
+    println(emptyList.none())
 }
