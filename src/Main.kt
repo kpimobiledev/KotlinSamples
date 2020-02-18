7e9bd2d1e547b2f6f59c095e56217c4c6a3d4275
lidaamber <lidaamber@gmail.com>
2020-02-18 22:11:27 +0200

    getting single item from collection

--- src/Main.kt
+++ src/Main.kt
@@ -1,34 +1,54 @@
-import java.lang.StringBuilder
-
 fun main() {
-    val countries = listOf("Ukraine", "USA", "Australia", "Austria", "Switzerland")
+    val namesList = arrayListOf("Alice", "Bob", "Caroline")
+    val optionalNamesSet = setOf("Alice", "Bob", null)
+    val gradesMap = mutableMapOf("Alice" to 100, "Bob" to 95)
 
-    // Group list items by first letter
-    val alphabetCountryMap = countries.groupBy { it.first() }.forEach { println(it.key) }
+    // Getting element by position in the indexed collections
+    val name = namesList.get(1)
+    val sameName = namesList[1]
+    val aliceGrade = gradesMap.get("Alice")
+    val sameAliceGrade = gradesMap["Alice"]
 
-    // Grouping by: grouping that supports functions on groups
+    // Getting element by position in non-indexed collection
+    val optionalName = optionalNamesSet.elementAt(1)
 
-    // Creation of map of Letter / countries count per letter
-    countries.groupingBy { it.first() }.eachCount().forEach { (t, u) -> println("$t, $u") }
+    // These conditional methods can return value even if the index is out of bounds
+    optionalNamesSet.elementAtOrElse(3) { "Daniel" }
+    optionalNamesSet.elementAtOrNull(4)
 
-    // Creates a map of Char to String, String is composed by fold() function
-    countries
-        .groupingBy { it.first() }
-        .fold("by default") { accumulator, element -> "$accumulator, $element" }
-        .forEach { (t, u) -> println("$t, $u") }
+    // Getting first and last elements by position
+    optionalNamesSet.first()
+    namesList.last()
 
-    // Creates a map of Char to String, String is composed by reduce() function
-    countries
-        .groupingBy { it.first() }
-        .reduce { _, acc, element -> "$acc, $element" }
-        .forEach { (t, u) -> println("$t, $u") }
+    // Getting first element matching the condition
+    optionalNamesSet.first { it != null && it.contains('o') }
 
-    // Creates a map of Char to aggregated String composed by aggregate() function
-    countries
-        .groupingBy { it.first() }
-        .aggregate { key, accumulator: StringBuilder?, element, first ->
-            return@aggregate if (first) StringBuilder("By $key we have $element")
-            else accumulator!!.append(", $element")
-        }
-        .forEach { (t, u) -> println("$t, $u") }
+    // Getting last element matching the condition
+    namesList.last { it.length > 3 }
+
+    // Also these methods have nullable variations in case there's no match
+    // firstOrNull() is the same as find()
+    namesList.firstOrNull { !it.contains('b') }
+    namesList.find { !it.contains('b') }
+
+    // lastOrNull() is the same as findLast()
+    namesList.lastOrNull { it.toUpperCase() != it }
+    namesList.findLast { it.toUpperCase() != it }
+
+    // Getting random element from the collection
+    println(namesList.random())
+
+    // Checking if element is in the collection
+    val element = "Caroline"
+    println(element in namesList)
+    println(namesList.contains(element))
+
+    // Checking if a collection contains another collection elements
+    println(namesList.containsAll(listOf("Alice", "Daniel")))
+
+    // Checking emptiness of the collection
+    println(namesList.isEmpty())
+    println(namesList.isNotEmpty())
+
+
 }
