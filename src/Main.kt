d2fd0463c056dee71d5c482ef7be601e4851043c
lidaamber <lidaamber@gmail.com>
2020-02-18 22:36:27 +0200

    getting several items from collection

--- src/Main.kt
+++ src/Main.kt
@@ -1,54 +1,35 @@
 fun main() {
-    val namesList = arrayListOf("Alice", "Bob", "Caroline")
-    val optionalNamesSet = setOf("Alice", "Bob", null)
-    val gradesMap = mutableMapOf("Alice" to 100, "Bob" to 95)
+    val namesList = arrayListOf("Alice", "Bob", "Caroline", "Daniel", "Ewan")
 
-    // Getting element by position in the indexed collections
-    val name = namesList.get(1)
-    val sameName = namesList[1]
-    val aliceGrade = gradesMap.get("Alice")
-    val sameAliceGrade = gradesMap["Alice"]
+    // Slicing a collection to receive a smaller collection
+    println(namesList.slice(1..4))
+    println(namesList.slice(0 until namesList.size step 2))
+    println(namesList.slice(setOf(1, 2, 3)))
 
-    // Getting element by position in non-indexed collection
-    val optionalName = optionalNamesSet.elementAt(1)
+    // Getting part of collection by taking and dropping first/last values from it
+    println(namesList.take(2))
+    println(namesList.takeLast(4))
+    println(namesList.drop(1))
+    println(namesList.dropLast(3))
 
-    // These conditional methods can return value even if the index is out of bounds
-    optionalNamesSet.elementAtOrElse(3) { "Daniel" }
-    optionalNamesSet.elementAtOrNull(4)
+    // Getting part of collection by taking and dropping first/last values
+    // from it while the condition is met
+    println(namesList.takeWhile { it.length < 6 })
+    println(namesList.takeLastWhile { it.toUpperCase() == it })
+    println(namesList.dropWhile { it.endsWith('e') })
+    println(namesList.dropLastWhile { it.startsWith('K') })
 
-    // Getting first and last elements by position
-    optionalNamesSet.first()
-    namesList.last()
+    // Collection chunks: create a collection of smaller collections of predefined chunk size
+    val chunkedNames = namesList.chunked(2)
+    println(chunkedNames)
 
-    // Getting first element matching the condition
-    optionalNamesSet.first { it != null && it.contains('o') }
+    // Chunks with transformation made on the each chunk
+    println(namesList.chunked(3) { it.joinToString(" and ") })
 
-    // Getting last element matching the condition
-    namesList.last { it.length > 3 }
+    // Windowing: creation of all the possible ranges of the predefined size
+    println(namesList.windowed(3))
+    println(namesList.windowed(1, step = 2))
 
-    // Also these methods have nullable variations in case there's no match
-    // firstOrNull() is the same as find()
-    namesList.firstOrNull { !it.contains('b') }
-    namesList.find { !it.contains('b') }
-
-    // lastOrNull() is the same as findLast()
-    namesList.lastOrNull { it.toUpperCase() != it }
-    namesList.findLast { it.toUpperCase() != it }
-
-    // Getting random element from the collection
-    println(namesList.random())
-
-    // Checking if element is in the collection
-    val element = "Caroline"
-    println(element in namesList)
-    println(namesList.contains(element))
-
-    // Checking if a collection contains another collection elements
-    println(namesList.containsAll(listOf("Alice", "Daniel")))
-
-    // Checking emptiness of the collection
-    println(namesList.isEmpty())
-    println(namesList.isNotEmpty())
-
-
+    // Windowing with transformation made on each window
+    println(namesList.windowed(3) { it.joinToString(" and ")})
 }
